4980c4ff3d8027f61dcd31ae9e19070211001798
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-12 14:49:54 +0300

    create backend connect mongo && pin modal

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const port = 3000;
+const pinRoute = require("./routes/pins");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+app.use("/api/pins", pinRoute);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
